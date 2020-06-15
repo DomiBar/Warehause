a96265210ee5a31d9113b6f3298f896ed58a0124
Dominik Baran <dominik.baran1992@gmail.com>
2020-06-15 17:03:19 +0200

    Add add_items, sort_items, exe file

--- functions.py
+++ functions.py
@@ -1,5 +1,21 @@
 def get_items(items):
     print("Nazwa\t Ilość\t Jednostka\t Cena jednostkowa")
     print(f"{'-'*len('Nazwa')}\t {'-'*len('Ilość')}\t {'-'*len('Jednostka')}\t {'-'*len('Cena jednostkowa')}\t")
+    sort_items(items)
     for i in range(len(items)):
         print(f"{items[i]['name']}\t {items[i]['quantity']}\t {items[i]['unit']}     \t {items[i]['unit_price']}")
+
+def add_items(items):
+    name=input("Podaj nazwę produktu: ")
+    quantity=float(input("Podaj ilość: "))
+    unit_name=input("Podaj nazwę jednostki: ")
+    unit_price=float(input("Podaj cenę jednostkową: "))
+    items+=[{'name':name, 'quantity':quantity,"unit":unit_name,"unit_price":unit_price}]
+    
+def sort_items(items):
+    dict_temp={}
+    for i in range(1,len(items)):
+        while items[-i]["name"]<items[-i-1]["name"]:
+            dict_temp=items[-i-1]
+            items[-i-1]=items[-i]
+            items[-i]=dict_temp
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import functions
+import os
 
 command=None
 actions={1: "Wyjdź z programu",
-         2: "Pokaż produkty"
+         2: "Pokaż produkty",
+         3: "Dodaj produkt",
          }
-items=[{'name':"buk", 'quantity':23,"unit":'m3',"unit_price":180},
-       {'name':"dąb", 'quantity':11,"unit":'m3',"unit_price":210},
-       {'name':"sosna", 'quantity':5,"unit":'m3',"unit_price":140}
+items=[{'name':"dąb", 'quantity':11.00,"unit":'m3',"unit_price":210.00},
+       {'name':"buk", 'quantity':23.00,"unit":'m3',"unit_price":180.00},
+       {'name':"sosna", 'quantity':5.00,"unit":'m3',"unit_price":140.00}
        ]
 while command!=1:
     for action in actions:
         print(f"{action}. {actions[action]}")
     command=int(input("Co chcesz zrobić? "))
+    os.system("cls")
     if command==2:
         functions.get_items(items)
+    if command==3:
+        functions.add_items(items)
+    print()
+
 
 print("Kończę działanie programu. Do widzenia.")
