c6894712ff272553d13980edcb87fcd8e5d20cb3
Dominik Baran <dominik.baran1992@gmail.com>
2020-06-16 13:00:43 +0200

    Add additional functionality

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import csv
+from datetime import datetime
+
 def get_items(items):
     print("Nazwa\t Ilość\t Jednostka\t Cena jednostkowa")
     print(f"{'-'*len('Nazwa')}\t {'-'*len('Ilość')}\t {'-'*len('Jednostka')}\t {'-'*len('Cena jednostkowa')}\t")
@@ -20,12 +23,15 @@ def sort_items(items):
             items[-i-1]=items[-i]
             items[-i]=dict_temp
 
-def sell_items(items, product, sell_quantity):
+def sell_items(items, sold_items, product, sell_quantity, sell_price):
     for i in range(len(items)):
         if product==items[i]['name']:
             if sell_quantity<items[i]['quantity']:
                 items[i]['quantity']-=sell_quantity
                 print(f"Sprzedano {sell_quantity} {items[i]['unit']} {product}")
+                now=datetime.now()
+                now=now.strftime("%d/%m/%Y %H:%M:%S")
+                sold_items.append({'name':product, 'quantity': sell_quantity, 'unit':items[i]['unit'], 'unit_price':sell_price, 'date':now})
                 get_items(items)
                 break
             else:
@@ -34,3 +40,60 @@ def sell_items(items, product, sell_quantity):
                 
     else:
         print("Nie znaleziono podanego produktu w bazie")
+
+def get_operations(sold_items):
+    print("Data\t Nazwa\t Ilość\t Jednostka\t Cena jednostkowa")
+    print(f"{'-'*len('Data')}\t {'-'*len('Nazwa')}\t {'-'*len('Ilość')}\t {'-'*len('Jednostka')}\t {'-'*len('Cena jednostkowa')}\t")
+    for item in sold_items:
+        print(f"{item['date']}\t {item['name']}\t {item['quantity']}\t {item['unit']}     \t {item['unit_price']}")
+
+def get_costs(items):
+    return sum([value['quantity']*value['unit_price'] for value in items])
+
+def get_income(sold_items):
+    return sum([value['quantity']*value['unit_price'] for value in sold_items])
+
+def show_revenue(items, sold_items):
+    print(f"Koszty: {get_costs(items)}")
+    print(f"Przychody: {get_income(sold_items)}")
+    print(f"Zysk: {get_income(sold_items)-get_costs(items)}")
+
+def export_to_csv(items,sold_items):
+    file_1='magazyn.csv'
+    file_2='operations.csv'
+    try:
+        with open(file_1,'w', newline='') as csvfile:
+            fieldnames=['name','quantity','unit','unit_price']
+            writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writerow({'name':'Nazwa','quantity':'Ilość','unit':'Jednostka','unit_price':'Cena jednostkowa'})
+            writer.writerows(items)    
+    except:
+        print(f"!!!!!!Nie udało się otworzyć pliku {file_1} . Dane nie zostały zapisane.!!!!!!\nJeżeli plik jest otwarty w innym programie, zamknij go przed zapisem.")
+
+    try:
+        with open(file_2,'w', newline='') as csvfile:
+            fieldnames=['date','name','quantity','unit','unit_price']
+            writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writerow({'name':'Nazwa','quantity':'Ilość','unit':'Jednostka','unit_price':'Cena jednostkowa','date':'Data'})
+            writer.writerows(sold_items)    
+    except:
+        print(f"!!!!!!Nie udało się otworzyć pliku {file_1} . Dane nie zostały zapisane.!!!!!!\nJeżeli plik jest otwarty w innym programie, zamknij go przed zapisem.")
+
+def load_from_csv(items,sold_items):
+    file_1='magazyn.csv'
+    file_2='operations.csv'
+    with open(file_1, newline='') as csvfile:
+        fieldnames=['name','quantity','unit','unit_price']
+        reader=csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=';')
+        items.clear()
+        for row in reader:
+            if row['name']!='Nazwa':
+                items.append(row)
+
+    with open(file_2, newline='') as csvfile:
+        fieldnames=['date','name','quantity','unit','unit_price']
+        reader=csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=';')
+        sold_items.clear()
+        for row in reader:
+            if row['name']!='Nazwa':
+                sold_items.append({'name':row['name'],'quantity':row['quantity'],'unit':row['unit'],'unit_price':row['unit_price'],'date':row['date']})
--- main.py
+++ main.py
@@ -5,12 +5,17 @@ command=None
 actions={1: "Wyjdź z programu",
          2: "Pokaż produkty",
          3: "Dodaj produkt",
-         4: "Sprzedaj produkt"
+         4: "Sprzedaj produkt",
+         5: "Pokaż listę operacji",
+         6: "Pokaż zysk",
+         7: "Zapisz",
+         8: "Importuj"
          }
-items=[{'name':"dąb", 'quantity':11.00,"unit":'m3',"unit_price":210.00},
-       {'name':"buk", 'quantity':23.00,"unit":'m3',"unit_price":180.00},
-       {'name':"sosna", 'quantity':5.00,"unit":'m3',"unit_price":140.00}
+items=[{'name':'dąb', 'quantity':11.00,'unit':'m3','unit_price':210.00},
+       {'name':'buk', 'quantity':23.00,'unit':'m3','unit_price':180.00},
+       {'name':'sosna', 'quantity':5.00,'unit':'m3','unit_price':140.00}
        ]
+sold_items=[]
 while command!=1:
     for action in actions:
         print(f"{action}. {actions[action]}")
@@ -21,7 +26,19 @@ while command!=1:
     if command==3:
         functions.add_items(items)
     if command==4:
-        functions.sell_items(items,input("Podaj nazwę produktu "), float(input("Podaj sprzedawaną ilość ")))
+        functions.sell_items(items,sold_items,
+                             input("Podaj nazwę produktu "), 
+                             float(input("Podaj sprzedawaną ilość ")),
+                             float(input("Podaj cenę sprzedaży "))
+                            )
+    if command==5:
+        functions.get_operations(sold_items)
+    if command==6:
+        functions.show_revenue(items,sold_items)
+    if command==7:
+        functions.export_to_csv(items,sold_items)
+    if command==8:
+        functions.load_from_csv(items,sold_items)
     print()
 
 
